0db0c812f7f41aeb9d3d6e54661a72d434c8a064
wolflergf <wolflergf@gmail.com>
2023-10-29 16:19:59 +0000

    Change layout

--- web.py
+++ web.py
@@ -13,8 +13,10 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <strong>productivity</strong>.", unsafe_allow_html=True)
 
+st.text_input(label=" ", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,6 +25,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label=" ", placeholder="Add a new todo...",
-              on_change=add_todo, key="new_todo")
